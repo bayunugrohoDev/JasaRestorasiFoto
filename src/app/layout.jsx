fdc14146f24f6c50980949ecbd5c1eb04ba9019d
bayunugrohoDev <bayunugroho963@gmail.com>
2024-08-16 11:12:08 +0700

    add: added goggle console

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -8,12 +8,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Jasa Restorasi Foto",
   description: "Jasa Restorasi Foto | Terpercaya, Profesional, dan Termurah",
-  other: [
-    {
-      name: 'google-site-verification',
-      content: 'pOvKPLUckaDtVXFBjlH1M_fZj0J7VXP-lo_ALwKnye4',
-    },
-  ],
+  other: {
+    "google-site-verification": "pOvKPLUckaDtVXFBjlH1M_fZj0J7VXP-lo_ALwKnye4",
+  },
 };
 
 export default function RootLayout({ children }) {
@@ -21,12 +18,15 @@ export default function RootLayout({ children }) {
     <html lang="en" className="scroll-smooth">
       <head>
         <title>Jasa Restorasi Foto</title>
-        <meta name="description" content="Jasa Restorasi Foto Terpercaya, Profesional, dan Termurah" />
+        <meta
+          name="description"
+          content="Jasa Restorasi Foto Terpercaya, Profesional, dan Termurah"
+        />
       </head>
       <body className={`bg-gray-100 text-gray-800 ${inter.className}`}>
         <Header />
         <main>{children}</main>
-        <Footer/>
+        <Footer />
       </body>
     </html>
   );
